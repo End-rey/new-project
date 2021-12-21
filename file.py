5b4238b9928cec7fa1a1a08189729fbbb4c9733e
End-rey <andreyis2017@gmail.com>
2021-12-21 18:03:52 +0300

    Learning numpy module

--- file.py
+++ file.py
@@ -2,6 +2,7 @@ import numpy
 
 
 def reverse_arr():
+    # записываем массив в обратном порядке
     arr = input().strip().split(' ')
     a = numpy.array(arr, float)
     result = numpy.flip(a)
@@ -9,6 +10,7 @@ def reverse_arr():
 
 
 def concatenate_arr():
+    # соединение 2 массивов по оси 0
     n, m, p = map(int, input().strip().split())
     arr1 = numpy.array([input().split() for y in range(n)], int)
     arr2 = numpy.array([input().split() for y in range(m)], int)
@@ -16,13 +18,65 @@ def concatenate_arr():
 
 
 def zeros_ones():
+    # создаем матрицы заполненными 0 и 1, с заданной размерностью
     a = tuple(map(int, input().split()))
     print(numpy.zeros(a, dtype=numpy.int64))
     print(numpy.ones(a, dtype=numpy.int64))
 
 
-def main():
-    strip_test()
+def sum_prod():
+    # находим сумму по оси 0 потом произведение всех элементов суммы
+    n, m = map(int, input().split())
+    a = numpy.array([input().split() for i in range(n)], int)
+    summ = numpy.sum(a, axis=0)
+    print(numpy.prod(summ))
+
+
+def min_max():
+    # находим минимум по оси 1, потом максимальный элемент
+    n, m = map(int, input().split())
+    a = numpy.array([input().split() for i in range(n)], int)
+    minn = numpy.min(a, axis=1)
+    print(numpy.max(minn))
+
+
+def mean_var_std():
+    # среднее арифметическое, дисперсия и стандартное отклонение
+    n, m = map(int, input().split())
+    a = numpy.array([input().split() for i in range(n)], int)
+    print(numpy.mean(a, axis=1))
+    print(numpy.var(a, axis=0))
+    print(round(numpy.std(a), 11))
+
+
+def matrix_product():
+    # произведение матриц
+    n = int(input())
+    a = numpy.array([input().split() for i in range(n)], int)
+    b = numpy.array([input().split() for i in range(n)], int)
+    print(numpy.dot(a, b))
+
+
+def inner_outer():
+    # скалярное и outer произведения матриц
+    a = numpy.array(input().split(), int)
+    b = numpy.array(input().split(), int)
+    print(numpy.inner(a, b))
+    print(numpy.outer(a, b))
+
+
+def value_of_p_at_point_x():
+    # находит значение полинома в заданной точке
+    p = numpy.array(input().split(), float)
+    x = float(input())
+    print(numpy.polyval(p, x))
+
+
+def determinant():
+    # определитель матрицы
+    n = int(input())
+    a = numpy.array([input().split() for i in range(n)], float)
+    print(round(numpy.linalg.det(a), 2))
 
 
 def strip_test():
@@ -43,6 +97,10 @@ def strip_test():
     # use strip function to remove the symbols from both ends
     str6 = str5. strip(' $*+')
     print(" Stripping the '+', '*' and '$' symbols on both sides of the string is = ", str6)
+
+
+def main():
+    strip_test()
 
 
 if __name__ == "__main__":
