e96b1d9c9f7be4871f47be0692e22b405b1b7879
End-rey <andreyis2017@gmail.com>
2021-12-21 15:40:30 +0300

    Test the strip function

--- file.py
+++ file.py
@@ -22,7 +22,27 @@ def zeros_ones():
 
 
 def main():
-    zeros_ones()
+    strip_test()
+
+
+def strip_test():
+    strinput = "  $$$$$  No. 1 Welcome to JAVATPOINT!! No. 1 $$$ "
+    # use strip() function to remove the set of characters
+    res = strinput.strip(' $No. 10 !')  # store result into res variable
+    print(" Given string is: ", strinput)
+    print(" After removing the set of characters: ", res)
+
+    str3 = ' 1 11 111 111 1111 Learn Python Programming Tutorial 1111 111 11 1 '
+    str4 = str3. strip(' 1')
+    print(" n  Given string is ", str3)
+    print(" Stripping 1 from both ends of the string using strip ('1') function ", str4)
+
+    # define new string
+    str5 = '++++++Python Programming Tutorial****** $$$$$'
+    print("n Given string is = ", str5)
+    # use strip function to remove the symbols from both ends
+    str6 = str5. strip(' $*+')
+    print(" Stripping the '+', '*' and '$' symbols on both sides of the string is = ", str6)
 
 
 if __name__ == "__main__":
